c089fc310e515c4a3d3c528f449058f4e558bdd5
VirajS00 <virajshukla00@gmail.com>
2021-10-24 22:15:49 +0530

    added dots with js

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,1 +1,27 @@
-const themes = ['winterDay', 'summerDay', 'winterNight', 'summerNight'];
+const container = document.querySelector('.dots-container');
+
+const themes = [
+	{ name: 'winterDay', label: 'Winter Day' },
+	{ name: 'summerDay', label: 'Summer day' },
+	{ name: 'winterNight', label: 'Winter night' },
+	{ name: 'summerNight', label: 'Summer Night' }
+];
+
+const template = document.querySelector('template.dot');
+let clone = template.content.cloneNode(true);
+
+themes.forEach((theme) => {
+	const checkBox = clone.querySelector('.dot-check');
+	const dot = clone.querySelector('.dot');
+
+	checkBox.id = theme.name;
+
+	dot.id = theme.name;
+	dot.setAttribute('for', theme.name);
+	dot.setAttribute('data-tooltip-label', theme.label);
+
+	container.append(checkBox);
+	container.append(dot);
+
+	clone = template.content.cloneNode(true);
+});
