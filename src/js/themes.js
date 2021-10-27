dde902be8357685fe39e9ea3e043ee18d15d54b1
VirajS00 <virajshukla00@gmail.com>
2021-10-27 21:06:45 +0530

    added theme switching without localstorage

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,27 +1,46 @@
 const container = document.querySelector('.dots-container');
 
 const themes = [
-	{ name: 'winterDay', label: 'Winter Day' },
-	{ name: 'summerDay', label: 'Summer day' },
-	{ name: 'winterNight', label: 'Winter night' },
-	{ name: 'summerNight', label: 'Summer Night' }
+	{ name: 'winterDay', label: 'Winter Day', color: '#77d0ed' },
+	{ name: 'summerDay', label: 'Summer day', color: '#dbb592' },
+	{ name: 'winterNight', label: 'Winter night', color: '#0f4b83' },
+	{ name: 'summerNight', label: 'Summer Night', color: '#71560d' }
 ];
+
+let checkBox, dot;
+
+// let theme = localStorage.getItem('theme');
 
 const template = document.querySelector('template.dot');
 let clone = template.content.cloneNode(true);
 
 themes.forEach((theme) => {
-	const checkBox = clone.querySelector('.dot-check');
-	const dot = clone.querySelector('.dot');
+	checkBox = clone.querySelector('.dot-check');
+	dot = clone.querySelector('.dot');
 
 	checkBox.id = theme.name;
 
 	dot.id = theme.name;
 	dot.setAttribute('for', theme.name);
 	dot.setAttribute('data-tooltip-label', theme.label);
+	dot.style.backgroundColor = theme.color;
 
 	container.append(checkBox);
 	container.append(dot);
 
 	clone = template.content.cloneNode(true);
+});
+
+const checkBoxes = document.querySelectorAll('.dot-check');
+
+const changeTheme = (theme) => {
+	document.body.removeAttribute('class');
+	document.body.classList.add(theme);
+};
+
+checkBoxes.forEach((box) => {
+	box.addEventListener('change', () => {
+		const themeName = box.id;
+		changeTheme(themeName);
+	});
 });
